446fe819f73d0b53ff1749c42fe08f3b7793b3ff
timov <timovmartin@gmail.com>
2021-08-18 13:01:31 +0200

    asd asd

--- db/init.sql
+++ db/init.sql
@@ -5,3 +5,6 @@ CREATE TABLE client (
     sync_message character varying(255) NOT NULL,
     sync_date timestamp with time zone DEFAULT CURRENT_TIMESTAMP NOT NULL
 );
+
+INSERT INTO client (id, title, sync_state, sync_message, sync_date) VALUES 
+(1, 'Joxx', 'Not started', 'Not started')
