b7361d53aca82a35605c6a786279f0e8336fe04c
timov <timovmartin@gmail.com>
2021-08-18 13:11:43 +0200

    asdasd asdasd

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ const config = {
   db: {
     /* do not put password or any sensitive info here, done only for demo */
     host: env.DB_HOST || "hattie.db.elephantsql.com",
-    port: env.DB_PORT || "5432",
+    port: env.DB_PORT || "",
     user: env.DB_USER || "gzyqhklu",
     password: env.DB_PASSWORD || "8Qriv1NJdJ8BbwvruruI2iHt72eX7utF",
     database: env.DB_NAME || "gzyqhklu"
