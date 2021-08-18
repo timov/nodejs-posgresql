47b6df1e80290e74fdcd9fde22ff0510be3148ed
timov <timovmartin@gmail.com>
2021-08-18 12:27:00 +0200

    asd asd

---
+++ routes/clients.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const router = express.Router();
+const clients = require("../services/clients");
+
+/* GET clints listing. */
+router.get("/", async function (req, res, next) {
+  try {
+    res.json(await clients.getMultiple(req.query.page));
+  } catch (err) {
+    console.error(`Error while getting clients `, err.message);
+    res.status(err.statusCode || 500).json({ message: err.message });
+  }
+});
+
+/* POST clients */
+router.post("/", async function (req, res, next) {
+  try {
+    res.json(await clients.create(req.body));
+  } catch (err) {
+    console.error(`Error while posting clients `, err.message);
+    res.status(err.statusCode || 500).json({ message: err.message });
+  }
+});
+
+module.exports = router;
---
+++ services/clients.js
@@ -0,0 +1,88 @@
+const db = require("./db");
+const helper = require("../helper");
+const config = require("../config");
+
+async function getMultiple(page = 1) {
+  const offset = helper.getOffset(page, config.listPerPage);
+  const rows = await db.query(
+    "SELECT id, title, sync_state, sync_message, sync_date, FROM client",
+    [offset, config.listPerPage]
+  );
+  const data = helper.emptyOrRows(rows);
+  const meta = { page };
+
+  return {
+    data,
+    meta
+  };
+}
+
+function validateCreate(client) {
+  let messages = [];
+
+  console.log(client);
+
+  if (!client) {
+    messages.push("No object is provided");
+  }
+
+  if (!client.title) {
+    messages.push("Title is empty");
+  }
+
+  if (!client.sync_state) {
+    messages.push("SyncState is empty");
+  }
+
+  if (!client.sync_message) {
+    messages.push("SyncMessage is empty");
+  }
+
+  if (messages.length) {
+    let error = new Error(messages.join());
+    error.statusCode = 400;
+
+    throw error;
+  }
+}
+
+async function create(client) {
+  validateCreate(client);
+
+  const rows = await db.query(
+    "SELECT * FROM client WHERE title = $1"[client.title]
+  );
+
+  const data = helper.emptyOrRows(rows);
+
+  if (data) {
+    const result = await db.query(
+      "UPDATE client SET sync_state = $1, sync_message = $2, sync_date = $3 WHERE title = $4",
+      [client.sync_state, client.sync_message, sync_date, client.title]
+    );
+    let message = "Error in updating client";
+
+    if (result.length) {
+      message = "Client updated successfully";
+    }
+
+    return { message };
+  }
+
+  const result = await db.query(
+    "INSERT INTO client(title, sync_state, sync_message, sync_date) VALUES ($1, $2, $3, $4) RETURNING *",
+    [client.title, client.sync_state, client.sync_message, sync_date]
+  );
+  let message = "Error in creating client";
+
+  if (result.length) {
+    message = "Client created successfully";
+  }
+
+  return { message };
+}
+
+module.exports = {
+  getMultiple,
+  create
+};
