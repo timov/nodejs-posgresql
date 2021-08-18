ed158d69d046891fd2f14800e6116fabade9a18d
timov <timovmartin@gmail.com>
2021-08-18 12:57:19 +0200

    asdasd asdsad

--- db/init.sql
+++ db/init.sql
@@ -1,7 +1,7 @@
-CREATE TABLE client(
+CREATE TABLE client (
     id SERIAL PRIMARY KEY,
     title character varying(255) NOT NULL UNIQUE,
     sync_state character varying(255) NOT NULL,
-    sync_date timestamp with time zone,
-    sync_message timestamp with time zone
+    sync_message character varying(255) NOT NULL,
+    sync_date timestamp with time zone DEFAULT CURRENT_TIMESTAMP NOT NULL
 );
